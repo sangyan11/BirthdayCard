1e508153b91dd59005b5505149d9c19b9cecdf97
sangyan11 <sangyanbhagi@gmail.com>
2021-04-26 12:21:09 +0530

    Initial Commit

---
+++ app/src/main/java/com/sangyanbhagi/BirthdayGreetingActivity.kt
@@ -0,0 +1,18 @@
+package com.sangyanbhagi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BirthdayGreetingActivity : AppCompatActivity() {
+    companion object {
+        const val NAME_EXTRA = "name_extra"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_birthday_greeting)
+        val name = intent.getStringExtra(NAME_EXTRA)
+        birthdayGreeting.text = "Happy Birthday\n$name!"
+
+    }
+}
---
+++ app/src/main/java/com/sangyanbhagi/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.sangyanbhagi
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun createBirthdayCard(view: View) {}
+    fun createAnniversaryCard(view: View) {}
+
+
+}
